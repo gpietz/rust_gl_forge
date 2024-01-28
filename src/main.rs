0018302e57767530b22a5960524a96d624e7fdfa
Gunnar Pietz <gpietz@posteo.de>
2024-01-28 14:05:40 +0100

    Remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,12 @@ mod renderable;
 mod sdl_window;
 mod string_utils;
 
-use crate::gl_buffer::BufferObject;
-use crate::gl_types::{BufferType, BufferUsage, VertexAttributeType};
-use crate::gl_vertex::{VertexArrayObject, VertexAttribute};
 use crate::renderable::first_triangle::FirstTriangle;
 use crate::renderable::Renderable;
 use anyhow::Result;
-use cgmath::Vector3;
 use color::Color;
-use gl::types::{GLfloat, GLsizei};
 use sdl2::event::Event;
 use sdl_window::SdlWindow;
-use std::mem::size_of;
 
 fn main() -> Result<()> {
     let mut window = SdlWindow::new(800, 600, "RUST SDL 2024", true)?;
