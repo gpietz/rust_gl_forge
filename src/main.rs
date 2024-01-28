6a76c56930bbfad4fa64e251e53f22cfb4134359
Gunnar Pietz <gpietz@posteo.de>
2024-01-28 19:21:27 +0100

    Implement drawable toggling with F1 and F2 keys

--- src/main.rs
+++ src/main.rs
@@ -17,20 +17,46 @@ use crate::renderable::Renderable;
 use anyhow::Result;
 use color::Color;
 use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use sdl_window::SdlWindow;
 
 fn main() -> Result<()> {
     let mut window = SdlWindow::new(800, 600, "RUST SDL 2024", true)?;
     window.clear_color = Color::new(0.10, 0.10, 0.25, 1.0);
 
-    // let mut triangle = FirstTriangle::new()?;
-    // triangle.setup()?;
+    let mut drawables: Vec<Box<dyn Renderable>> = Vec::new();
+    add_drawable(&mut drawables, FirstTriangle::new);
+    add_drawable(&mut drawables, IndexedQuad::new);
 
-    let mut indexed_quad = IndexedQuad::new()?;
-    indexed_quad.setup()?;
+    // Set the initial drawable to the last one
+    let mut current_index = drawables.len().saturating_sub(1);
 
     'main_loop: loop {
         for event in window.event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'main_loop,
+                Event::KeyDown {
+                    keycode: Some(keycode),
+                    ..
+                } => match keycode {
+                    Keycode::Escape => break 'main_loop,
+                    Keycode::F1 => {
+                        // Logic for F1 key
+                        if current_index > 0 {
+                            current_index -= 1;
+                        } else {
+                            current_index = drawables.len() - 1;
+                        }
+                    }
+                    Keycode::F2 => {
+                        // Logic for F2 key
+                        current_index = (current_index + 1) % drawables.len();
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+
             if let Event::Quit { .. } = event {
                 break 'main_loop;
             }
@@ -38,11 +64,25 @@ fn main() -> Result<()> {
 
         window.clear();
 
-        //triangle.draw();
-        indexed_quad.draw();
+        // Draw the current active drawable
+        if let Some(drawable) = drawables.get_mut(current_index) {
+            drawable.draw();
+        }
 
         window.swap();
     }
 
     Ok(())
+}
+
+fn add_drawable<F, R, E>(drawables: &mut Vec<Box<dyn Renderable>>, creator: F)
+where
+    F: FnOnce() -> Result<R, E>,
+    R: Renderable + 'static, // Ensure R implements Renderable and has a static lifetime
+    E: std::fmt::Debug,      // E can be any type that implements Debug (for error handling)
+{
+    match creator() {
+        Ok(drawable) => drawables.push(Box::new(drawable)),
+        Err(e) => eprintln!("Failed to create drawable: {:?}", e),
+    }
 }
