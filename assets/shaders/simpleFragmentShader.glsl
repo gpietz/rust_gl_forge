114f574f89a5c1d836e8f08c58268cc5ab26792c
Gunnar Pietz <gpietz@posteo.de>
2024-02-01 20:38:00 +0100

    Optimize fragment shader

--- assets/shaders/simpleFragmentShader.glsl
+++ assets/shaders/simpleFragmentShader.glsl
@@ -3,15 +3,6 @@ out vec4 FragColor;
 in vec3 ourColor;
 uniform float time;
 
-vec3 interpolateColor(vec3 colorA, vec3 colorB, float factor) {
-        return mix(colorA, colorB, factor);
-}
-
-float noise(vec2 st) {
-        // Implement a basic noise function here
-        return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
-}
-
 void main()
 {
     if (time > 0) {
@@ -26,11 +17,10 @@ void main()
         // Color interpolation
         vec3 colorA = vec3(1.0, 0.5, 0.0); // orange
         vec3 colorB = vec3(0.0, 0.5, 1.0); // blue
-        vec3 dynamicColor = interpolateColor(colorA, colorB, wave);
+        vec3 dynamicColor = mix(colorA, colorB, wave);
         
         // Combine effects
         vec3 finalColor = mix(dynamicColor, ourColor, wave2);
-        
         
         FragColor = vec4(finalColor, 1.0);
     }
