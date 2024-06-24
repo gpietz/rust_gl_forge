a2394312799bc733308a6a27fd9022ce7f24e2a5
Gunnar Pietz <gpietz@posteo.de>
2024-06-24 22:57:23 +0200

    Several improvements in BlendGuard

--- shared_lib/src/opengl/blend_guard.rs
+++ shared_lib/src/opengl/blend_guard.rs
@@ -1,41 +1,109 @@
+use anyhow::Result;
+use gl::types::GLenum;
+
+use crate::check_gl_panic;
+use crate::gl_utils::check_gl_error;
+
 pub struct BlendGuard {
     original_blend: bool,
+    blend_src: GLenum,
+    blend_dest: GLenum,
+    callback: Option<Box<dyn Fn(bool) -> bool>>,
 }
 
 impl BlendGuard {
-    fn new() -> Self {
-        let original_blend: bool = unsafe { gl::IsEnabled(gl::BLEND) == gl::TRUE };
+    pub fn new(blend_src: GLenum, blend_dest: GLenum) -> Result<Self> {
+        let mut original_blend = false;
         unsafe {
+            original_blend = gl::IsEnabled(gl::BLEND) == gl::TRUE;
             gl::Enable(gl::BLEND);
-            gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+            check_gl_panic!("Error enabling GL blend");
+            gl::BlendFunc(blend_src, blend_dest);
+            check_gl_panic!("Error calling blend function");
         }
-        BlendGuard { original_blend }
+        check_gl_error()?;
+        Ok(BlendGuard {
+            original_blend,
+            blend_src,
+            blend_dest,
+            callback: None,
+        })
     }
 
-    pub fn enable(&self) {
+    pub fn enable(&self) -> Result<()> {
+        if self.call_callback(true) {
+            unsafe {
+                gl::Enable(gl::BLEND);
+                gl::BlendFunc(self.blend_src, self.blend_dest);
+            }
+            check_gl_error()?;
+        }
+        Ok(())
+    }
+
+    pub fn disable(&self) -> Result<()> {
+        if self.call_callback(false) {
+            unsafe {
+                gl::Disable(gl::BLEND);
+            }
+            check_gl_error()?;
+        }
+        Ok(())
+    }
+
+    pub fn set_blend_func(&mut self, src: GLenum, dest: GLenum) -> Result<()> {
         unsafe {
-            gl::Enable(gl::BLEND);
-            gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+            gl::BlendFunc(src, dest);
+            check_gl_panic!("Error calling blend function");
+        }
+        check_gl_error()?;
+        self.blend_src = src;
+        self.blend_dest = dest;
+        Ok(())
+    }
+
+    pub fn get_blend_func(&self) -> (GLenum, GLenum) {
+        (self.blend_src, self.blend_dest)
+    }
+
+    pub fn set_callback<F: 'static + Fn(bool) -> bool>(&mut self, callback: F) {
+        self.callback = Some(Box::new(callback));
+    }
+
+    pub fn clear_callback(&mut self) {
+        self.callback = None;
+    }
+
+    fn call_callback(&self, enable: bool) -> bool {
+        match &self.callback {
+            Some(cb) => cb(enable),
+            None => true,
         }
     }
 
-    pub fn disable(&self) {
-        unsafe {
-            gl::Disable(gl::BLEND);
+    pub fn is_enabled(&self) -> bool {
+        unsafe { 
+            gl::IsEnabled(gl::BLEND) == gl::TRUE 
         }
     }
 }
 
 impl Default for BlendGuard {
     fn default() -> Self {
-        Self::new()
+        Self::new(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA)
+            .expect("Failed to create default BlendGuard")
     }
 }
 
 impl Drop for BlendGuard {
     fn drop(&mut self) {
         if !self.original_blend {
-            self.disable();
+            if let Err(e) = self.disable() {
+                eprintln!("Error disabling blend in BlendGuard: {}", e);
+            }
         }
     }
 }
+
+unsafe impl Send for BlendGuard {}
+unsafe impl Sync for BlendGuard {}
