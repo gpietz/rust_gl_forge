0bb0630c46e4ad73e99081b85a96bf18e715e237
Gunnar Pietz <gpietz@posteo.de>
2024-07-17 12:17:57 +0200

    Refactor and improve VertexArrayObject

--- shared_lib/src/opengl/vertex_array_object.rs
+++ shared_lib/src/opengl/vertex_array_object.rs
@@ -1,8 +1,10 @@
-use crate::gl_prelude::check_gl_error;
-use anyhow::{anyhow, Result};
-use gl::types::GLint;
+use std::ffi::c_void;
+
+use anyhow::Result;
+use gl::types::{GLint, GLsizei, GLuint};
 
 use crate::gl_traits::{Bindable, Deletable};
+use crate::opengl::vertex_attribute::VertexAttribute;
 
 //////////////////////////////////////////////////////////////////////////////
 // - Vertex Array Object (VAO) -
@@ -10,80 +12,97 @@ use crate::gl_traits::{Bindable, Deletable};
 
 pub struct VertexArrayObject {
     id: u32,
+    layout: Option<Vec<VertexAttribute>>,
 }
 
 impl VertexArrayObject {
     /// Create a new Vertex Array Object and bind it.
-    pub fn new() -> Result<VertexArrayObject> {
-        let vao = VertexArrayObject::create_vao()?;
-        unsafe {
-            gl::BindVertexArray(vao.id);
-            check_gl_error();
-        }
-        Ok(vao)
-    }
-
-    /// Create a new Vertex Array Object without binding it.
-    pub fn new_without_bind() -> Result<VertexArrayObject> {
-        VertexArrayObject::create_vao()
-    }
-
-    fn create_vao() -> Result<VertexArrayObject> {
+    pub fn new() -> Self {
         let mut id = 0;
         unsafe {
             gl::GenVertexArrays(1, &mut id);
-            if id == 0 {
-                return Err(anyhow!("Failed to generate a vertex array object"));
-            }
-            check_gl_error()?;
         }
-        Ok(VertexArrayObject {
+        Self {
             id,
-        })
+            layout: None,
+        }
     }
 
-    pub fn bind(&self) -> Result<()> {
+    pub fn bind(&self) {
         unsafe {
             gl::BindVertexArray(self.id);
         }
-        check_gl_error()
     }
 
-    pub fn unbind(&self) -> Result<()> {
+    pub fn unbind() {
         unsafe {
             gl::BindVertexArray(0);
         }
-        check_gl_error()
-    }
-
-    /// Returns the identifier of the vertex array.
-    pub fn array_id(&self) -> u32 {
-        self.id
-    }
-}
-
-impl Bindable for VertexArrayObject {
-    fn bind(&self) -> Result<()> {
-        unsafe {
-            gl::BindVertexArray(self.id);
-        }
-        check_gl_error()
-    }
-
-    fn unbind(&self) -> Result<()> {
-        unsafe {
-            gl::BindVertexArray(0);
-        }
-        check_gl_error()
     }
 
     fn is_bound(&self) -> Result<bool> {
         let mut current_vao = 0;
         unsafe {
             gl::GetIntegerv(gl::VERTEX_ARRAY_BINDING, &mut current_vao);
-            check_gl_error()?;
         }
         Ok(current_vao == self.id as GLint)
+    }
+
+    /// Returns the identifier of the vertex array.
+    pub fn array_id(&self) -> u32 {
+        self.id
+    }
+
+    pub fn set_layout<T: AsRef<[VertexAttribute]>>(&mut self, attributes: T) {
+        let attributes = attributes.as_ref().to_vec();
+        self.layout = Some(attributes.clone());
+
+        self.bind();
+        for (i, attr) in attributes.iter().enumerate() {
+            let normalized = if let Some(n) = attr.normalized {
+                if n {
+                    gl::TRUE
+                } else {
+                    gl::FALSE
+                }
+            } else {
+                gl::FALSE
+            };
+            let stride = attr.stride.unwrap_or(0) as GLsizei;
+            let offset = attr.offset.unwrap_or(0) as *const c_void;
+            let type_ = attr.data_type.to_gl_enum();
+            let size = attr.data_type.size();
+
+            unsafe {
+                gl::VertexAttribPointer(
+                    i as GLuint,
+                    attr.components as GLint,
+                    type_,
+                    normalized,
+                    stride,
+                    offset,
+                );
+                gl::EnableVertexAttribArray(i as GLuint);
+            }
+        }
+        Self::unbind();
+    }
+
+    pub fn clear_layout(&mut self) {
+        if let Some(attributes) = &self.layout {
+            self.bind();
+            for (i, attr) in attributes.iter().enumerate() {
+                unsafe {
+                    gl::DisableVertexAttribArray(i as GLuint);
+                }
+            }
+            self.layout = None;
+            Self::unbind();
+        }
+    }
+    
+    pub fn has_layout(&self) -> bool {
+         self.layout.is_some()
     }
 }
 
